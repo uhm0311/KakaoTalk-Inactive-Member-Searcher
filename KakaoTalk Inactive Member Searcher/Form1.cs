079fafbeae1b448965abbaaaf13086358da8208d
uhm0311 <uhm0311@naver.com>
2020-07-31 19:02:53 +0900

    Initial commit.

---
+++ KakaoTalk Inactive Member Searcher/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KakaoTalk_Inactive_Member_Searcher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
